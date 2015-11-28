a498e2b31e2f61867606ade05ad19303de811c8f
Matthew N <6504408+matthewngai@users.noreply.github.com>
2015-11-28 09:42:50 -0500

    removed unecesary checks

--- app/src/main/java/bitpot/aboutcanada/AboutCanadaFragment.java
+++ app/src/main/java/bitpot/aboutcanada/AboutCanadaFragment.java
@@ -109,9 +109,7 @@ public class AboutCanadaFragment extends android.support.v4.app.Fragment {
                     tv.setTextColor(Color.parseColor(getResources().getString(R.string.font_body)));
                     tv.setPadding((int) (15 * getResources().getDisplayMetrics().density), 0, 0, 0);
                 }
-                else if (line.substring(0,3).equals("") || line.substring(0, 3).equals("   ")){
-
-                } else {
+                 else {
                     tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
                     tv.setTextColor(Color.parseColor(getResources().getString(R.string.font_body)));
                     tv.setPadding((int) (15 * getResources().getDisplayMetrics().density), 0, 0, 0);
