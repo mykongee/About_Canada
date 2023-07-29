f79165c32a3b4ebce27e2bd8370d27a86b611190
Group project <null>
2023-07-29 15:30:08 -0700

    sanity checks on string length

--- app/src/main/java/bitpot/aboutcanada/AboutCanadaFragment.java
+++ app/src/main/java/bitpot/aboutcanada/AboutCanadaFragment.java
@@ -80,7 +80,7 @@ public class AboutCanadaFragment extends android.support.v4.app.Fragment {
 
 
     public void DispFileText(LinearLayout ll){
-        InputStream is = getResources().openRawResource(R.raw.filename);
+        InputStream is = getResources().openRawResource(R.raw.social);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line;
         String entireFile = "";
@@ -89,25 +89,36 @@ public class AboutCanadaFragment extends android.support.v4.app.Fragment {
             while((line = br.readLine()) != null) { // <--------- place readLine() inside loop
                 // entireFile += (line + "\n"); // <---------- add each line to entireFile
                 TextView tv = new TextView(getActivity());
-                tv.setText(line.substring(3, line.length()));
+                if (line.length() > 0) {
+                    tv.setText(line.substring(3, line.length()));
+                }
                 ViewGroup.LayoutParams layout = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                if (line.length() <= 0) {
+
+                }
+                else {
+
 
                 if (line.substring(0,3).equals("H--")) {
                     tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
                     tv.setTextColor(Color.parseColor(getResources().getString(R.string.font_header)));
                     tv.setPadding((int) (15 * getResources().getDisplayMetrics().density), 0, (int) (5 * getResources().getDisplayMetrics().density), (int) (5 * getResources().getDisplayMetrics().density));
                 }else if (line.substring(0,3).equals("P--")) {
-                    tv.setText("   • "+line.substring(3,line.length()));
+                    tv.setText(" • "+line.substring(3,line.length()));
                     tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
                     tv.setTextColor(Color.parseColor(getResources().getString(R.string.font_body)));
                     tv.setPadding((int) (15 * getResources().getDisplayMetrics().density), 0, 0, 0);
-                }else {
+                }
+                else if (line.substring(0,3).equals("") || line.substring(0, 3).equals("   ")){
+
+                } else {
                     tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
                     tv.setTextColor(Color.parseColor(getResources().getString(R.string.font_body)));
                     tv.setPadding((int) (15 * getResources().getDisplayMetrics().density), 0, 0, 0);
                 }
                 tv.setLayoutParams(layout);
                 ll.addView(tv);
+            }
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
